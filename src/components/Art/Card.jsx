b6231d18814cf46969764a681db37e8da0a37abf
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-03 17:22:55 -0700

    added link by id to detail in card.jsx

--- src/components/Art/Card.jsx
+++ src/components/Art/Card.jsx
@@ -1,14 +1,20 @@
 import styles from '../../App.css';
+import { Link } from 'react-router-dom';
 
 export default function ArtCard({ art }) {
   return (
-    <div className={styles.card}>
-      <img className={styles.img}
-        src={`https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`}
-        alt={'Artworks'}
-      />
-      <p className={styles.title}>{art.title}</p>
-      <p className={styles.artist}>{art.artist_title}</p>
-    </div>
+    <>
+      <Link to={`/art/${art.id}`}>
+        <div className={styles.card}>
+          <img
+            className={styles.img}
+            src={`https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`}
+            alt={'Artworks'}
+          />
+          <p className={styles.title}>{art.title}</p>
+          <p className={styles.artist}>{art.artist_title}</p>
+        </div>
+      </Link>
+    </>
   );
 }
