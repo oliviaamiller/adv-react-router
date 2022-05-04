aa62ae4a72880f19c819a709a65e26ac01bc4188
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-03 17:18:21 -0700

    wrote second test for navigating to detail page

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 describe('Art Router', () => {
   it('renders a list of art works', async () => {
     render(<App />);
 
+    //check to see that loading message appears
     screen.getByText(/loading.../i);
 
+    //check that list images have rendered from api call
     await screen.findAllByAltText('Artworks');
+  });
+
+  it('should navigate to a detail page', async () => {
+    render(
+      <MemoryRouter initialEntries={['/art']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    //check to find link 
+    const link = await screen.findByText('Homesickness');
+    
+    //user clicks on link
+    userEvent.click(link);
+
+    //check to see that more data is displaying
+    await screen.findByText('Belgium');
   });
 });
